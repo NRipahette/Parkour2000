de4dab3840be164c60d5b1d5a1f6319a2563e216
Noé Ripahette <noe.ripahette@gmail.com>
2022-01-28 17:37:14 +0100

    Wallrun presque terminé

--- Parkour2000/Assets/Scripts/PlayerController.cs
+++ Parkour2000/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Vector3 PlayerVelocity;
+    public float playerHeight = 2f;
+    public float playerwidth = 1f;
     public float GravtiyForce = 20f;
     public float GroundCheckDistance = 0.1f;
+    public float WallCheckDistance = 0.5f;
     public float JumpForce = 90f;
+    public float WallRunForce = 10f;
     public float VitesseMax = 10f;
+    public float VitesseMaxWallRun = 8f;
     public float Friction = 20f;
+    public float WallFriction = 10f;
     public float AirAcceleration = 1f;
     public float SprintMultiplier = 2.5f;
-    float pSpeed;
     public bool IsGrounded;
     public bool WasGrounded;
     public bool IsSprinting;
     public bool IsOnSlope;
+    public bool IsTouchingAWall;
+    public bool IsAttachedToWall;
+    public bool IsJumping;
+    private bool useGravity;
     private float CheckGroundDelay = 0.05f;
     private Vector3 m_GroundNormal;
     private float m_LastTimeJumped;
     private float SlopeForce = 600f;
     private float SlopeForceRayLength = 1f;
     private CharacterController playerController;
+    private BoxCollider WallRunHitboxCollider;
+    private Collider wallHit;
+    private RaycastHit RayWall;
+    private GameObject cameraFPS;
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +44,11 @@ public class PlayerController : MonoBehaviour
         WasGrounded = true;
         IsSprinting = false;
         IsOnSlope = false;
+        useGravity = true;
+        IsJumping = false;
+        WallRunHitboxCollider = transform.Find("WallRunHitbox").GetComponent<BoxCollider>();
         playerController = GetComponent<CharacterController>();
+        cameraFPS = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
@@ -54,14 +71,14 @@ public class PlayerController : MonoBehaviour
         //Vector3 targetUpwardVelocity = JumpForce *  Vector3.up ;
         Vector3 targetVelocity = targetForwardVelocity + targetLateralVelocity;
 
-
         GroundCheck();
-        if (WasGrounded || IsGrounded)
+        if (IsGrounded)
         {
-
+            useGravity = false;
             if (IsSprinting)
             {
                 targetForwardVelocity *= SprintMultiplier;
+
                 targetVelocity = targetForwardVelocity + targetLateralVelocity;
                 PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
             }
@@ -79,6 +96,7 @@ public class PlayerController : MonoBehaviour
                 PlayerVelocity += Vector3.up * JumpForce;
                 m_LastTimeJumped = Time.time;
                 IsGrounded = false;
+                IsJumping = true;
             }
 
             // Slope Check 
@@ -89,30 +107,57 @@ public class PlayerController : MonoBehaviour
             }
             else
                 IsOnSlope = false;
+        }
+        else if (IsAttachedToWall)
+        {
 
+            PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, transform.forward * Input.GetAxis("Vertical") * VitesseMaxWallRun, Time.deltaTime * WallFriction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, transform.forward * Input.GetAxis("Vertical") * VitesseMaxWallRun, Time.deltaTime * WallFriction).z);
+            //PlayerVelocity += targetForwardVelocity * WallRunForce * Time.deltaTime;
+            //PlayerVelocity += -RayWall.normal * WallRunForce / 5 * Time.deltaTime; // Colle le joueur au mur
+            PlayerVelocity = Vector3.ProjectOnPlane(PlayerVelocity, RayWall.normal); // Colle le joueur au mur
+            //PlayerVelocity = Vector3.ClampMagnitude(PlayerVelocity, VitesseMaxWallRun);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(Vector3.Dot(cameraFPS.transform.forward, Vector3.up) > 0.866f) // Si regard vers le haut alors saute uniquement vers le haut proche du mur
+                {
+                    PlayerVelocity += (Vector3.up * JumpForce);
 
+                }
+                else
+                {
+                    PlayerVelocity += RayWall.normal * JumpForce + (Vector3.up * JumpForce); // Sinon saute en s'éloignant du mur
+                }
+                useGravity = true;
+                IsAttachedToWall = false;
+                wallHit = null;
+            }
 
+        }
+        else if (IsTouchingAWall)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartWallRun(wallHit);
 
-
+            }
         }
         else
         {
-
+            IsAttachedToWall = false;
             PlayerVelocity += new Vector3(targetVelocity.x * Time.deltaTime * AirAcceleration, 0, targetVelocity.z * Time.deltaTime * AirAcceleration);
-            //Appliquer la gravité
+            //Gravité
             PlayerVelocity += Vector3.down * GravtiyForce * Time.deltaTime;
-
-
-
         }
+
         playerController.Move(PlayerVelocity * Time.deltaTime);
         HeadBumpCheck();
         WasGrounded = IsGrounded;
     }
 
+
     void GroundCheck()
     {
-
+        IsJumping = false;
         // reset values before the ground check
         IsGrounded = false;
         m_GroundNormal = Vector3.up;
@@ -120,7 +165,7 @@ public class PlayerController : MonoBehaviour
         // only try to detect ground if it's been a short amount of time since last jump; otherwise we may snap to the ground instantly after we try jumping
         if (Time.time >= m_LastTimeJumped + CheckGroundDelay)
         {
-            Debug.DrawLine(transform.position - Vector3.up, (transform.position - Vector3.up) + Vector3.down * (GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude), Color.red, 10f);
+            Debug.DrawLine(transform.position - Vector3.up, (transform.position - Vector3.up * playerHeight / 2) + Vector3.down * (GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude), Color.red, 10f);
             // if we're grounded, collect info about the ground normal with a downward capsule cast representing our character capsule
             if (Physics.Raycast(transform.position - Vector3.up, Vector3.down, out RaycastHit hit, GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude))
             {
@@ -145,13 +190,94 @@ public class PlayerController : MonoBehaviour
     }
     void HeadBumpCheck()
     {
-        Debug.DrawLine(transform.position + Vector3.up, (transform.position + Vector3.up) + Vector3.up * GroundCheckDistance, Color.red, 10f);
+        Debug.DrawLine(transform.position + Vector3.up, (transform.position + Vector3.up * playerHeight / 2) + Vector3.up * GroundCheckDistance, Color.red, 10f);
         // if we're grounded, collect info about the ground normal with a downward capsule cast representing our character capsule
         if (Physics.Raycast(transform.position + Vector3.up, Vector3.up, out RaycastHit hit, GroundCheckDistance))
         {
             PlayerVelocity = new Vector3(PlayerVelocity.x, -0.5f, PlayerVelocity.z);
         }
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("obstacle"))
+        {
+            wallHit = other;
+            IsTouchingAWall = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("obstacle"))
+        {
+            IsTouchingAWall = false;
+
+        }
+    }
+
+
+    //void WallCheck()
+    //{
+
+    //    // reset values before the ground check
+    //    IsTouchingAWall = false;
+    //    AttachedWallNormal = Vector3.zero;
+
+    //    Debug.DrawLine(transform.position, transform.position + transform.right * (playerwidth / 2 + WallCheckDistance), Color.blue, 10f);
+    //    Debug.DrawLine(transform.position, transform.position - transform.right * (playerwidth / 2 + WallCheckDistance), Color.blue, 10f);
+    //    // if we're grounded, collect info about the ground normal with a downward capsule cast representing our character capsule
+    //    if (wallHit)
+    //    {
+    //        IsTouchingAWall = true;
+    //        // storing the normal of the wall we collided to
+    //        AttachedWallNormal = hit.normal;
+
+    //        // Only consider this a valid ground hit if the ground normal goes in the same direction as the character up
+    //        // and if the slope angle is lower than the character controller's limit
+    //        if (Vector3.Dot(hit.normal, transform.up) > 0f)
+    //        {
+
+
+    //            //// handle snapping to the ground
+    //            //if (hit.distance > m_Controller.skinWidth)
+    //            //{
+    //            //    m_Controller.Move(Vector3.down * hit.distance);
+    //            //}
+    //        }
+    //    }
+    //    else if (IsWallLeft)
+    //    {
+    //        IsTouchingAWall = true;
+    //        // storing the normal of the wall we collided to
+    //        AttachedWallNormal = hit2.normal;
+    //    }
+    //    else
+    //        StopWallRun();
+
+
+    //}
+
+    void StartWallRun(Collider wallHit)
+    {
+        IsAttachedToWall = true;
+        useGravity = false;
+        PlayerVelocity = new Vector3(PlayerVelocity.x, 0, PlayerVelocity.z);
+        wallHit.Raycast(new Ray(transform.position, wallHit.ClosestPoint(transform.position) - transform.position), out RayWall, 5f);
+        if (PlayerVelocity.magnitude <= VitesseMaxWallRun)
+        {
+            PlayerVelocity += transform.forward * WallRunForce * Time.deltaTime;
+
+            PlayerVelocity -= RayWall.normal * WallRunForce / 5 * Time.deltaTime;
+        }
+    }
+
+    void StopWallRun()
+    {
+        IsAttachedToWall = false;
+        useGravity = true;
     }
 
     //Calcul de pente 
@@ -162,3 +288,4 @@ public class PlayerController : MonoBehaviour
     }
 
 }
+
