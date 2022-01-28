0a9d4c29edb9e53cddcd9c0017d00defa262fc58
Phromm <harrdoise@hotmail.fr>
2022-01-28 17:44:48 +0100

    Update init

--- Parkour2000/Assets/Scripts/PlayerController.cs
+++ Parkour2000/Assets/Scripts/PlayerController.cs
@@ -4,150 +4,306 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector3 PlayerVelocity;
-    public float GravtiyForce = 20f;
-    public float GroundCheckDistance = 0.1f;
-    public float JumpForce = 90f;
-    public float VitesseMax = 10f;
-    public float Friction = 20f;
-    public float AirAcceleration = 1f;
-    public float SprintMultiplier = 2.5f;
-    float pSpeed;
-    public bool IsGrounded;
-    public bool WasGrounded;
-    public bool IsSprinting;
-    public bool IsOnSlope;
-    private float CheckGroundDelay = 0.05f;
-    private Vector3 m_GroundNormal;
-    private float m_LastTimeJumped;
-    private float SlopeForce = 600f;
-    private float SlopeForceRayLength = 1f;
-    private CharacterController playerController;
+	public Vector3 PlayerVelocity;
+	public float CurrentSpeed;
+	public float GravityForce = 20f;
+	public float GroundCheckDistance = 0.1f;
+	public float JumpForce;
+	public float baseJumpForce;
+	public float SlopeJumpForce;
+	public float VitesseMax = 10f;
+	public float Friction;
+	public float SlidingFriction;
+	public float baseFriction;
+	public float AirAcceleration = 1f;
+	public float SprintMultiplier = 2.5f;
+	public float CrouchMultiplier = 0.5f;
+	public float SlidingMultiplier = 2f;
+	private float standingHeight = 1.8f;
+	private float crouchingHeight = 1.2f;
+	public float interpolationCrouchFrames = 45f;
+	public Vector3 crouchingView;
+	public Vector3 slidingView;
+	public Vector3 standingView;
+	public bool IsGrounded;
+	public bool WasGrounded;
+	public bool IsSprinting;
+	public bool IsCrouching;
+	public bool IsSliding;
+	public bool IsOnSlope;
+	private bool startedSliding;
+	private float CheckGroundDelay = 0.05f;
+	public Vector3 m_GroundNormal;
+	private float m_LastTimeJumped;
+	public float SlopeForce = 1000f;
+	private float SlopeForceRayLength = 1f;
+	private CharacterController playerController;
+	public GameObject playerView;
+	public Material slide_m;
+	public Material normal_m;
+	private MeshRenderer renderer;
+	private float slidingStartTime;
+	public float slidingLength;
+	float elapsedFrames;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        IsGrounded = true;
-        WasGrounded = true;
-        IsSprinting = false;
-        IsOnSlope = false;
-        playerController = GetComponent<CharacterController>();
-    }
+	public Vector3 temp;
+	public Vector3 groundSlopeDir;
+	public float groundSlopeAngle;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            IsSprinting = true;
-        }
-        else
-        {
-            IsSprinting = false;
-        }
+	// Start is called before the first frame update
+	void Start()
+	{
+		IsGrounded = true;
+		WasGrounded = true;
+		IsSprinting = false;
+		IsOnSlope = false;
+		playerController = GetComponent<CharacterController>();
+		renderer = GetComponent<MeshRenderer>();
+		IsCrouching = false;
+		IsSliding = false;
+		Friction = baseFriction;
+		elapsedFrames = float.Epsilon;
+		startedSliding = false;
+	}
 
-        // calculate the desired velocity from inputs, max speed, and current slope
-        Vector3 targetForwardVelocity = Input.GetAxis("Vertical") * transform.forward * VitesseMax;
+	// Update is called once per frame
+	void Update()
+	{
+		CurrentSpeed = Mathf.Abs(PlayerVelocity.magnitude);
+		if (Input.GetKey(KeyCode.LeftShift) && !IsSliding)
+		{
+			IsSprinting = true;
+		}
+		else
+		{
+			IsSprinting = false;
+		}
 
-        Vector3 targetLateralVelocity = Input.GetAxis("Horizontal") * transform.right * VitesseMax;
+		if (Input.GetKey(KeyCode.W) && !IsSliding)
+		{
+			IsCrouching = true;
 
-        //Vector3 targetUpwardVelocity = JumpForce *  Vector3.up ;
-        Vector3 targetVelocity = targetForwardVelocity + targetLateralVelocity;
+		}
+		else
+		{
+			IsCrouching = false;
+		}
 
-
-        GroundCheck();
-        if (WasGrounded || IsGrounded)
-        {
-
-            if (IsSprinting)
-            {
-                targetForwardVelocity *= SprintMultiplier;
-                targetVelocity = targetForwardVelocity + targetLateralVelocity;                
-                PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
-            }
-            else
-            {
-                PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
-            }
-            // start by canceling out the vertical component of our velocity
-            PlayerVelocity = new Vector3(PlayerVelocity.x, 0f, PlayerVelocity.z);
-            //Jump        
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-
-                // then, add the jumpSpeed value upwards
-                PlayerVelocity += Vector3.up * JumpForce;
-                m_LastTimeJumped = Time.time;
-                IsGrounded = false;
-            }
-
-            // Slope Check 
-            if (m_GroundNormal != Vector3.up)
-            {
-                IsOnSlope = true;
-                PlayerVelocity += Vector3.down * SlopeForce * Time.deltaTime;
-            }
-            else
-                IsOnSlope = false;
-
-            
-
-            
-           
-        }
-        else
-        {
-
-            PlayerVelocity += new Vector3(targetVelocity.x * Time.deltaTime * AirAcceleration, 0, targetVelocity.z * Time.deltaTime * AirAcceleration);
-            //Appliquer la gravité
-            PlayerVelocity += Vector3.down * GravtiyForce * Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.LeftAlt) && !IsCrouching)
+		{
+			IsSliding = true;
+		}
 
 
-
-        }
-        playerController.Move(PlayerVelocity * Time.deltaTime);
-        WasGrounded = IsGrounded;
-    }
-
-    void GroundCheck()
-    {
-
-        // reset values before the ground check
-        IsGrounded = false;
-        m_GroundNormal = Vector3.up;
-
-        // only try to detect ground if it's been a short amount of time since last jump; otherwise we may snap to the ground instantly after we try jumping
-        if (Time.time >= m_LastTimeJumped + CheckGroundDelay)
-        {
-            Debug.DrawLine(transform.position - Vector3.up, (transform.position - Vector3.up) + Vector3.down * (GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude), Color.red, 10f);
-            // if we're grounded, collect info about the ground normal with a downward capsule cast representing our character capsule
-            if (Physics.Raycast(transform.position - Vector3.up, Vector3.down, out RaycastHit hit, GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude))
-            {
-                IsGrounded = true;
-                // storing the upward direction for the surface found
-                m_GroundNormal = hit.normal;
-
-                // Only consider this a valid ground hit if the ground normal goes in the same direction as the character up
-                // and if the slope angle is lower than the character controller's limit
-                if (Vector3.Dot(hit.normal, transform.up) > 0f)
-                {
+		//if (IsSliding && Time.time - slidingStartTime >= slidingLength)
+		//{
+		//	float t = slidingLength / 10.0f;
+		//	Friction = baseFriction;
+		//	IsSliding = false;
+		//	CurrentSpeed = VitesseMax * CrouchMultiplier;
+		//	renderer.material = normal_m;
+		//	Friction = Mathf.Lerp(Friction, baseFriction, 0.001f);
+		//}
+		////if (IsSliding && Friction > baseFriction - 0.01f)
+		////{
+		////	IsSliding = false;
+		////	Friction = baseFriction;
+		////	renderer.material = normal_m;
+		////}
+		//else if (CurrentSpeed >= VitesseMax * SlidingMultiplier && IsCrouching && !IsSliding)
+		//{
+		//	slidingStartTime = Time.time;
+		//	IsSliding = true;
+		//	Friction = SlidingFriction;
+		//	renderer.material = slide_m;
+		//}
 
 
-                    //// handle snapping to the ground
-                    //if (hit.distance > m_Controller.skinWidth)
-                    //{
-                    //    m_Controller.Move(Vector3.down * hit.distance);
-                    //}
-                }
-            }
-        }
-    }
 
-    //Calcul de pente 
-    public Vector3 GetDirectionReorientedOnSlope(Vector3 direction, Vector3 slopeNormal)
-    {
-        Vector3 directionRight = Vector3.Cross(direction, transform.up);
-        return Vector3.Cross(slopeNormal, directionRight).normalized;
-    }
+
+		// calculate the desired velocity from inputs, max speed, and current slope
+		Vector3 targetForwardVelocity = Input.GetAxis("Vertical") * transform.forward * VitesseMax;
+
+		Vector3 targetLateralVelocity = Input.GetAxis("Horizontal") * transform.right * VitesseMax;
+
+		//Vector3 targetUpwardVelocity = JumpForce *  Vector3.up ;
+		Vector3 targetVelocity = targetForwardVelocity + targetLateralVelocity;
+
+
+		float t;
+		if (IsCrouching)
+		{
+			t = (float)elapsedFrames / interpolationCrouchFrames;
+			playerView.transform.localPosition = Vector3.Lerp(playerView.transform.localPosition, crouchingView, 0.01f);
+			elapsedFrames = (elapsedFrames + 1) % (interpolationCrouchFrames + 1f);
+		}
+		else if(!IsSliding)
+		{
+			t = (float)elapsedFrames / interpolationCrouchFrames;
+			playerView.transform.localPosition = Vector3.Lerp(playerView.transform.localPosition, standingView, 0.01f);
+			elapsedFrames = (elapsedFrames + 1) % (interpolationCrouchFrames + 1f);
+		}
+
+
+		
+
+		GroundCheck();
+		if (IsGrounded)
+		{
+			temp = Vector3.Cross(m_GroundNormal, Vector3.down);
+			groundSlopeDir = Vector3.Cross(temp, m_GroundNormal);
+			groundSlopeAngle = Vector3.Angle(m_GroundNormal, Vector3.up);
+			//transform.Rotate(transform.right,groundSlopeAngle);
+			transform.rotation = Quaternion.Euler(new Vector3(-groundSlopeAngle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
+
+			if (IsSliding && !startedSliding)
+			{
+				slidingStartTime = Time.time;
+				startedSliding = true;
+
+				t = (float)elapsedFrames / interpolationCrouchFrames;
+				playerView.transform.localPosition = Vector3.Lerp(playerView.transform.localPosition, slidingView, 0.01f);
+				elapsedFrames = (elapsedFrames + 1) % (interpolationCrouchFrames + 1f);
+
+				targetForwardVelocity *= SlidingMultiplier;
+				targetVelocity = targetForwardVelocity + targetLateralVelocity;
+				PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
+
+			}
+			else if (startedSliding && Time.time - slidingStartTime < slidingLength)
+			{
+				t = (float)elapsedFrames / interpolationCrouchFrames;
+				playerView.transform.localPosition = Vector3.Lerp(playerView.transform.localPosition, slidingView, 0.01f);
+				elapsedFrames = (elapsedFrames + 1) % (interpolationCrouchFrames + 1f);
+
+				targetForwardVelocity *= SlidingMultiplier;
+				targetVelocity = targetForwardVelocity + targetLateralVelocity;
+				PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
+			}
+			else if (startedSliding && Time.time - slidingStartTime >= slidingLength)
+			{
+				startedSliding = false;
+				IsSliding = false;
+
+				t = (float)elapsedFrames / interpolationCrouchFrames;
+				playerView.transform.localPosition = Vector3.Lerp(playerView.transform.localPosition, standingView, 0.01f);
+				elapsedFrames = (elapsedFrames + 1) % (interpolationCrouchFrames + 1f);
+			}
+
+			if (IsSprinting)
+			{
+				targetForwardVelocity *= SprintMultiplier;
+				targetVelocity = targetForwardVelocity + targetLateralVelocity;
+				PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
+			}
+			if (IsCrouching)
+			{
+				if (IsSliding)
+				{
+					targetForwardVelocity *= SlidingMultiplier;
+					targetLateralVelocity *= SlidingMultiplier;
+					targetVelocity = targetForwardVelocity + targetLateralVelocity;
+					PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
+
+				}
+				else
+				{
+					targetForwardVelocity *= CrouchMultiplier;
+					targetLateralVelocity *= CrouchMultiplier;
+					targetVelocity = targetForwardVelocity + targetLateralVelocity;
+					PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
+
+				}
+			}
+			else
+			{
+				PlayerVelocity = new Vector3(Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).x, PlayerVelocity.y, Vector3.Lerp(PlayerVelocity, targetVelocity, Time.deltaTime * Friction).z);
+			}
+
+			// start by canceling out the vertical component of our velocity
+			PlayerVelocity = new Vector3(PlayerVelocity.x, 0f, PlayerVelocity.z);
+
+			// Slope Check 
+			if (m_GroundNormal != Vector3.up)
+			{
+				IsOnSlope = true;
+				//JumpForce = SlopeJumpForce;
+				PlayerVelocity += Vector3.down * SlopeForce * Time.deltaTime;
+				Debug.DrawLine(transform.position, transform.position + PlayerVelocity, Color.green);
+			}
+			else
+			{
+				IsOnSlope = false;
+				//JumpForce = baseJumpForce;
+			}
+			//Jump        
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				// start by canceling out the vertical component of our velocity
+				PlayerVelocity = new Vector3(PlayerVelocity.x, 0f, PlayerVelocity.z);
+
+				// then, add the jumpSpeed value upwards
+				PlayerVelocity += Vector3.up * JumpForce;
+				m_LastTimeJumped = Time.time;
+				IsGrounded = false;
+			}
+
+		}
+		else
+		{
+
+			PlayerVelocity += new Vector3(targetVelocity.x * Time.deltaTime * AirAcceleration, 0, targetVelocity.z * Time.deltaTime * AirAcceleration);
+			//Appliquer la gravité
+			PlayerVelocity += Vector3.down * GravityForce * Time.deltaTime;
+
+		}
+		PlayerVelocity = Vector3.ClampMagnitude(PlayerVelocity, VitesseMax * 2);
+
+		playerController.Move(PlayerVelocity * Time.deltaTime);
+		WasGrounded = IsGrounded;
+
+
+	}
+
+	void GroundCheck()
+	{
+
+		// reset values before the ground check
+		IsGrounded = false;
+		m_GroundNormal = Vector3.up;
+		
+		// only try to detect ground if it's been a short amount of time since last jump; otherwise we may snap to the ground instantly after we try jumping
+		if (Time.time >= m_LastTimeJumped + CheckGroundDelay)
+		{
+			Debug.DrawLine(transform.position - Vector3.up, (transform.position - Vector3.up) + Vector3.down * (GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude), Color.red, 10f);
+			// if we're grounded, collect info about the ground normal with a downward capsule cast representing our character capsule
+			if (Physics.Raycast(transform.position - Vector3.up, Vector3.down, out RaycastHit hit, GroundCheckDistance + (PlayerVelocity * Time.deltaTime).magnitude))
+			{
+				IsGrounded = true;
+				// storing the upward direction for the surface found
+				m_GroundNormal = hit.normal;
+
+				// Only consider this a valid ground hit if the ground normal goes in the same direction as the character up
+				// and if the slope angle is lower than the character controller's limit
+				if (Vector3.Dot(hit.normal, transform.up) > 0f)
+				{
+
+
+					//// handle snapping to the ground
+					//if (hit.distance > m_Controller.skinWidth)
+					//{
+					//    m_Controller.Move(Vector3.down * hit.distance);
+					//}
+				}
+			}
+		}
+	}
+
+	//Calcul de pente 
+	public Vector3 GetDirectionReorientedOnSlope(Vector3 direction, Vector3 slopeNormal)
+	{
+		Vector3 directionRight = Vector3.Cross(direction, transform.up);
+		return Vector3.Cross(slopeNormal, directionRight).normalized;
+	}
 
 }
